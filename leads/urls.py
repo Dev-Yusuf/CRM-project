9b4a21f648c12282e67254846698e265be55418b
Dev-Yusuf <devyusuf01@gmail.com>
2022-07-22 07:20:30 -0700

    added more features

--- leads/urls.py
+++ leads/urls.py
@@ -6,6 +6,8 @@ app_name = 'leads'
 urlpatterns = [
     path('', views.lead_list, name='lead_list'),
     path('<int:pk>/', views.lead_detail, name='lead-detail'),
+    path('leads/create/', views.lead_create, name='lead-create'),
+    path('<int:pk>/update/', views.lead_update, name='lead-update'),
 
 ]
 
